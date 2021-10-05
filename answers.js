8df0acd0ab5b813cb95802faf05eb5d86f61fdc9
Violet Stewart <vkazoo2@gmail.com>
2021-10-05 13:45:32 -0700

    set up basic html and css and started js

---
+++ answers.js
@@ -0,0 +1,9 @@
+export const answers = [
+    'Signs point to yes',
+    'Absolutely not and you should be embaressed for asking!',
+    'Maybe?!',
+    'Yes',
+    'No',
+    'Ask again l8r ',
+    'Heck yes princess'
+];
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // import functions and grab DOM elements
-
+import { answers } from "./answers.js";
 // initialize global state
+const getRandomNumber = (max)=> {
+  return answers[Math.floor(math.random()*answers.length)]
+};
 
+const inputQuestion = document.getElementById('input-question');
+const answerButton = document.getElementById('answer-button');
+const randomAnswer = document.getElementById('random-answer');
 // set event listeners 
   // get user input
   // use user input to update state 
