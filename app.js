3f89f1e876fc297a83deb7447e4afe1cda7e9f41
Violet Stewart <vkazoo2@gmail.com>
2021-10-05 16:02:17 -0700

    forgot to commit for too long, made so many changes

--- app.js
+++ app.js
@@ -2,12 +2,38 @@
 import { answers } from "./answers.js";
 // initialize global state
 const getRandomNumber = (max)=> {
-  return answers[Math.floor(math.random()*answers.length)]
+  return answers[Math.floor(Math.random()*answers.length)];
 };
 
 const inputQuestion = document.getElementById('input-question');
 const answerButton = document.getElementById('answer-button');
 const randomAnswer = document.getElementById('random-answer');
+const resetButton = document.getElementById('reset-button');
+
+function shake () {
+  var answerButton = document.getElementById("answer-button")
+
+answerButton.classList.add('shake')
+setTimeout(function(){answerButton.classList.remove('shake');}, 1000 )
+setTimeout(function(){answers();}, 1000)
+
+}
+
+answerButton.addEventListener('click', ()=> {
+  const randomIndex = getRandomNumber(answers.length);
+  randomAnswer.textContent = randomIndex;
+
+  //console.log(randomIndex)
+});
+
+resetButton.addEventListener('click', ()=> {
+  randomAnswer.textContent = '';
+  inputQuestion.value = '';
+});
+
+
+
+
 // set event listeners 
   // get user input
   // use user input to update state 
